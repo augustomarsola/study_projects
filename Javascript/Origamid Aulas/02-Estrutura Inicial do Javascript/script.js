3e584ef5055b0e186b46e1ccf062c9a18b634904
augusto.marsola <augusto.marsola@mps.com.br>
2020-05-23 23:29:42 -0300

    Adicionado efeito de scroll Suave

--- Javascript/Origamid Aulas/02-Estrutura Inicial do Javascript/script.js
+++ Javascript/Origamid Aulas/02-Estrutura Inicial do Javascript/script.js
@@ -67,5 +67,27 @@ function scrollSuave() {
     link.addEventListener('click', scrollToSection);
   });
 }
-
 scrollSuave();
+
+function animaScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      if (isSectionVisible)
+        section.classList.add('ativo');
+      else
+        section.classList.remove('ativo');
+    });
+  }
+
+  if (sections.length) {
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+
+animaScroll();
