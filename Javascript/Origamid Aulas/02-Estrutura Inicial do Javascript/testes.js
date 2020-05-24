8d326ae7a90c10c878e92373fad0f2aea87bd7a3
augustomarsola <gugumarsola@hotmail.com>
2020-05-24 18:23:07 -0300

    Finalizado o estudo sobre arrays com js

--- Javascript/Origamid Aulas/02-Estrutura Inicial do Javascript/testes.js
+++ Javascript/Origamid Aulas/02-Estrutura Inicial do Javascript/testes.js
@@ -634,33 +634,78 @@
 
 // // Number
 
-// Retorne um número aleatório
-// entre 1050 e 2000
-const randomNum = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
-console.log(randomNum);
+// // Retorne um número aleatório
+// // entre 1050 e 2000
+// const randomNum = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
+// console.log(randomNum);
 
-// Retorne o maior número da lista abaixo
-const numeros = '4, 5, 20, 8, 9';
-const numerosArray = numeros.split(',');
-const numeroMax = Math.max(...numerosArray);
-console.log(numeroMax);
+// // Retorne o maior número da lista abaixo
+// const numeros = '4, 5, 20, 8, 9';
+// const numerosArray = numeros.split(',');
+// const numeroMax = Math.max(...numerosArray);
+// console.log(numeroMax);
 
-// Crie uma função para limpar os preços
-// e retornar os números com centavos arredondados
-// depois retorne a soma total
-const listaPrecos = ['R$ 59,99', ' R$ 100,222',
-  'R$ 230  ', 'r$  200'
-];
-let valorTotal = 0;
+// // Crie uma função para limpar os preços
+// // e retornar os números com centavos arredondados
+// // depois retorne a soma total
+// const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+//   'R$ 230  ', 'r$  200'
+// ];
+// let valorTotal = 0;
 
-listaPrecos.forEach((preco) => {
-  valorTotal += limparPreco(preco);
-});
+// listaPrecos.forEach((preco) => {
+//   valorTotal += limparPreco(preco);
+// });
 
-function limparPreco(preco) {
-  preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
-  preco = +preco.toFixed(2);
-  return preco;
-}
+// function limparPreco(preco) {
+//   preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
+//   preco = +preco.toFixed(2);
+//   return preco;
+// }
 
-console.log(valorTotal);
+// console.log(valorTotal);
+
+
+// --------
+
+
+// // Arrays
+
+// Remova o primeiro valor de comidas e coloque em uma variável
+// Remova o último valor de comidas e coloque em uma variável
+// Adicione 'Arroz' ao final da array
+// Adicione 'Peixe' e 'Batata' ao início da array
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+const primeiraComida = comidas.shift();
+const ultimaComida = comidas.pop();
+comidas.push('Arroz');
+comidas.unshift('Peixe', 'Batata');
+console.log(comidas, primeiraComida, ultimaComida);
+
+// Arrume os estudantes em ordem alfabética
+// Inverta a ordem dos estudantes
+// Verifique se Joana faz parte dos estudantes
+// Verifique se Juliana faz parte dos estudantes
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+console.log(estudantes.sort());
+console.log(estudantes.reverse());
+console.log(estudantes.includes('Joana'));
+console.log(estudantes.includes('Juliana'));
+
+
+// Substitua section por ul e div com li,
+// utilizando split e join
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`;
+html = html.split('section').join('ul').split('div').join('li');
+console.log(html);
+
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+const carrosOriginal = carros.splice();
+carros.pop();
+console.log(carros);
