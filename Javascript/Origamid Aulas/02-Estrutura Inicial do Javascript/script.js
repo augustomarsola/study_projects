58df92636c97a82aa7da0000a3721e478c321155
augusto.marsola <augusto.marsola@mps.com.br>
2020-05-23 23:14:14 -0300

    Finalizado scroll Suave interno

--- Javascript/Origamid Aulas/02-Estrutura Inicial do Javascript/script.js
+++ Javascript/Origamid Aulas/02-Estrutura Inicial do Javascript/script.js
@@ -40,3 +40,32 @@ function initAccodrion() {
   }
 }
 initAccodrion();
+
+function scrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    // Formo alternativa
+    // const topo = section.offsetTop;
+
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: "smooth",
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+
+scrollSuave();
