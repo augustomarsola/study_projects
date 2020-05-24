452412a521f1cc6200bb24916e59fdd093eea5ee
augusto.marsola <augusto.marsola@mps.com.br>
2020-05-23 22:55:41 -0300

    Criado efeito accordion com js puro

--- Javascript/Origamid Aulas/02-Estrutura Inicial do Javascript/script.js
+++ Javascript/Origamid Aulas/02-Estrutura Inicial do Javascript/script.js
@@ -1,8 +1,16 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function iniTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add('ativo');
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
+    });
+  }
 
   function activeTab(index) {
     tabContent.forEach((content) => {
@@ -10,10 +18,25 @@ if (tabMenu.length && tabContent.length) {
     });
     tabContent[index].classList.add('ativo');
   }
-
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    });
-  });
 }
+iniTabNav();
+
+function initAccodrion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const classeAtivo = 'ativo';
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(classeAtivo);
+    accordionList[0].nextElementSibling.classList.add(classeAtivo);
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+
+  function activeAccordion() {
+    this.classList.toggle(classeAtivo);
+    this.nextElementSibling.classList.toggle(classeAtivo);
+  }
+}
+initAccodrion();
