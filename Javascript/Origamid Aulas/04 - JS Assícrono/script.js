36435764bc6d0e12a30a4b8e9ce07cd9abf81484
augustomarsola <gugumarsola@hotmail.com>
2020-05-29 16:36:36 -0300

    Iniciando manipulação usando Fecth

--- Javascript/Origamid Aulas/04 - JS Assícrono/script.js
+++ Javascript/Origamid Aulas/04 - JS Assícrono/script.js
@@ -29,24 +29,53 @@
 
 // console.log(retorno);
 
-const login = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('Usuário Logado');
-  }, 1000);
-});
+// const login = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve('Usuário Logado');
+//   }, 1000);
+// });
 
-const dados = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('Dados Carregados');
-  }, 1500);
-});
+// const dados = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve('Dados Carregados');
+//   }, 1500);
+// });
 
-// // Resolve todos ou espera 1 dar erro
-// const carregouTudo = Promise.all([login, dados]);
+// // // Resolve todos ou espera 1 dar erro
+// // const carregouTudo = Promise.all([login, dados]);
 
-// // Espera o primeiro acontecer e já finaliza
-const carregouTudo = Promise.race([login, dados]);
+// // // Espera o primeiro acontecer e já finaliza
+// const carregouTudo = Promise.race([login, dados]);
 
-carregouTudo.then(resolucao => {
-  console.log(resolucao);
-})
+// carregouTudo.then(resolucao => {
+//   console.log(resolucao);
+// });
+
+const doc = fetch('./doc.txt');
+
+doc
+  .then(r => r.text())
+  .then(body => {
+    const conteudo = document.querySelector('.conteudo');
+    conteudo.innerText = body;
+  });
+
+const cep = fetch('https://viacep.com.br/ws/01001000/json/');
+
+cep
+  .then(r => r.json())
+  .then(body => {
+    const conteudo = document.querySelector('.cep');
+    conteudo.innerText = body.logradouro;
+  });
+
+const sobre = fetch('./sobre.html');
+const div = document.createElement('div');
+
+sobre
+  .then(r => r.text())
+  .then(body => {
+    div.innerHTML = body;
+    const titulo = div.querySelector('h1');
+    document.querySelector('h1').innerText = titulo.innerText;
+  });
