ee2e10c0c08faa006ede912f408df202be88ff4a
augustomarsola <augusto.marsola@live.com>
2020-06-14 00:48:49 -0300

    Limpeza para manipulação da página validar CPF

--- Javascript/Origamid Aulas/11 - Regexp/validar cpf/script.js
+++ Javascript/Origamid Aulas/11 - Regexp/validar cpf/script.js
@@ -1,10 +1,1 @@
-import ValidarCpf from './validar-cpf.js';
-
-const cpf = document.querySelector('#cpf');
-const validarCpf = new ValidarCpf(cpf).iniciar();
-
-const validadorCPF = new ValidarCpf();
-
-console.log(validadorCPF.validar('111 111 111 222'))
-
-console.log(validarCpf)
+import ValidarCpf from "./validar-cpf.js";
