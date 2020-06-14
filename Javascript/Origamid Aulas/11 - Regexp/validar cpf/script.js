1e6de615bd45d4bd257cd302fb5323c9b25aa530
augustomarsola <augusto.marsola@live.com>
2020-06-14 13:44:46 -0300

    Finalizado validador de exemplo utilizando Regexp

--- Javascript/Origamid Aulas/11 - Regexp/validar cpf/script.js
+++ Javascript/Origamid Aulas/11 - Regexp/validar cpf/script.js
@@ -1,1 +1,10 @@
 import ValidarCpf from "./validar-cpf.js";
+
+const cpf = document.querySelector("#cpf");
+const validarCpf = new ValidarCpf(cpf).inciar();
+
+const validadorCpf = new ValidarCpf();
+
+console.log(validadorCpf.validar("111 222 333 33"));
+
+console.log(validarCpf);
